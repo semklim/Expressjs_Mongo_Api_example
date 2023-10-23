60ccea8a509d9d6d98fc04a8ee894b334e8256c0
Roman Klymashenko <romanklimashenko@gmail.com>
2023-10-23 21:07:16 +0300

    dell cors rules

--- corsOptions/corsOptions.js
+++ corsOptions/corsOptions.js
@@ -1,12 +1,13 @@
-const whitelist = ['https://expressjs-mongo-api-example.vercel.app'];
+const whitelist = ['https://expressjs-mongo-api-example.vercel.app', 'expressjs-mongo-api-example-ngawnuz97-semklim.vercel.app', ''];
 
 module.exports = corsOptions = {
   origin: (origin, callback) => {
     // Check if the origin is in the whitelist or if it's a same-origin request
-    if (whitelist.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
+    // if (whitelist.includes(origin) || !origin) {
+    //   callback(null, true);
+    // } else {
+    //   callback(new Error('Not allowed by CORS'));
+    // }
+    callback(null, true);
   },
 };
