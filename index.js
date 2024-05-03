fa7e267a5e26a1b37c70a9abbdd0afffce04609e
Romeo <104751985+semklim@users.noreply.github.com>
2024-05-03 13:44:31 +0300

    test

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 app.use("/api", apiRouter);
 app.use(compression());
 app.use(express.static(path.join(process.cwd(), "public")));
